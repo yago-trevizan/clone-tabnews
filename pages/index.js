52d3df77e19d9b80b8dd11e449a33a5a4a8a2f67
yago-trevizan <yago.ta@hotmail.com>
2025-02-24 22:34:29 +0000

    Atualização da frase principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Título Oficial</h1>
+    return (
+        <div>
+            <h1>Título Oficial atualizado e muito mais legal do que o anterior.</h1>
+
+            <p>Parágrafo simples sendo exibido através do --amend e do --force</p>
+        </div>
+    );
 }
 
 export default Home;
