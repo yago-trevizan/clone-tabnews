ddf5ac69c17d4879f80ce040822d9b0c535679ff
yago-trevizan <yago.ta@hotmail.com>
2025-02-27 23:08:08 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
-    return (
-        <div>
-            <h1>Título Oficial atualizado e muito mais legal do que o anterior.</h1>
+  return (
+    <div>
+      <h1>Título Oficial atualizado e muito mais legal do que o anterior.</h1>
 
-            <h1>Parágrafo simples sendo exibido através do --amend</h1>
-        </div>
-    );
+      <h1>Parágrafo simples sendo exibido através do --amend</h1>
+    </div>
+  );
 }
 
 export default Home;
