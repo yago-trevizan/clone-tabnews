c7111d16dbffcda2291ee1f540f46a4b3bcac291
yago-trevizan <yago.ta@hotmail.com>
2025-04-27 16:56:00 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response
-    .status(200)
-    .json({ chave: "Os alunos do curso.dev são acima da média." });
+  const queryVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = queryVersionResult.rows[0].server_version;
+
+  const queryMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = queryMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const queryOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const openedConnections = queryOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
