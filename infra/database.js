6e124a52d66f7dc3f97d94db313b85d4a65ff30b
yago-trevizan <yago.ta@hotmail.com>
2025-12-08 20:31:07 +0000

    fix: handle client as undefined in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -9,10 +9,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n Erro dentro do catch do database.js:");
     console.error(error);
-    throw Error;
+    throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
