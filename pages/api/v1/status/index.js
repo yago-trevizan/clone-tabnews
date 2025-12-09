464a14470870aa11dba92898b952c12ca784465d
yago-trevizan <yago.ta@hotmail.com>
2025-12-09 19:32:22 +0000

    refactor: use `next-connect` in `/migrations` and `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,47 +1,41 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const queryVersionResult = await database.query("SHOW server_version;");
-    const databaseVersion = queryVersionResult.rows[0].server_version;
+router.get(getHandler);
 
-    const queryMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
-    const maxConnections = queryMaxConnectionsResult.rows[0].max_connections;
+export default router.handler(controller.errorsHandlers);
 
-    const databaseName = process.env.POSTGRES_DB;
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
 
-    const queryOpenedConnectionsResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-      values: [databaseName],
-    });
+  const queryVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = queryVersionResult.rows[0].server_version;
 
-    const openedConnections = queryOpenedConnectionsResult.rows[0].count;
+  const queryMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = queryMaxConnectionsResult.rows[0].max_connections;
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: databaseVersion,
-          max_connections: parseInt(maxConnections),
-          opened_connections: openedConnections,
-        },
+  const databaseName = process.env.POSTGRES_DB;
+
+  const queryOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const openedConnections = queryOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
