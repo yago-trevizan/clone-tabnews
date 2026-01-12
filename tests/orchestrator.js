198fa01f4d4f4c428628392681b13e52efd7baa9
yago-trevizan <yago.ta@hotmail.com>
2026-01-12 12:40:38 +0000

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -92,6 +92,12 @@ async function getLastEmail() {
   return lastEmailItem;
 }
 
+function extractUUID(text) {
+  const match = text.match(/[0-9a-fA-F-]{36}/);
+
+  return match ? match[0] : null;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -100,6 +106,7 @@ const orchestrator = {
   createSession,
   deleteAllEmails,
   getLastEmail,
+  extractUUID,
 };
 
 export default orchestrator;
