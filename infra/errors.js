a740179d8f6b419e8b9a4585fb0aae5a8b5b7c12
yago-trevizan <yago.ta@hotmail.com>
2026-02-26 17:28:01 +0000

    refactor: improve error logging in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -20,14 +20,15 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
 
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -36,6 +37,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
